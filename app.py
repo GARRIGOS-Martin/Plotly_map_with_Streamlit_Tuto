8e14c876bcd975a3766864099b98feeb222b6960
martiiiData <martin.garrigos@iiidata.fr>
2022-10-11 17:49:12 +0200

    V1 11/10/2022, il faudra revoir l'archi et esthetique pour passer en V2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 import pandas as pd
 import streamlit as st
 import plotly.express as px
@@ -5,7 +6,6 @@ import json
 from urllib.request import urlopen
 
 # On charge les données 
-
 df = pd.read_csv('./data/data_tuto_streamlit.csv', sep = ';')
 with urlopen('https://france-geojson.gregoiredavid.fr/repo/departements.geojson') as f : 
     geo_dep = json.load(f)
@@ -15,17 +15,9 @@ state_id_map = {}
 for feature in geo_dep['features']:
  feature['id'] = feature['properties']['code']
  state_id_map[feature['properties']['nom']] = feature['id']
-
-
 st.title('Tutoriel Streamlit')
 st.header('Créer un dashboard de visualisations spatiales avec Streamlit et Plotly ')
-
-#list_indicateurs = list(df.columns[2:])
-#indicateur = st.sidebar.selectbox("Selectionner un indicateur :", list_indicateurs)
-
 viz, comparatif = st.tabs(['Visualiser les indicateurs', 'Comparer les indicateurs entre départements'])
-
-
 with viz : 
     # La selectbox prend en paramètres des Dataframe alors ne nous privons pas ! 
     # Je crée un sous dataframe qui contient seulement les colonnes d'indicateurs
@@ -34,11 +26,7 @@ with viz :
     list_indicateurs = list_indicateurs.insert(0, "Aucun")
     # Je crée la selectbox ET je stocke la valeur dans une variable (important pour la suite)
     indicateur = st.selectbox('Choisissez un indicateur à visualiser', list_indicateurs)
-
-
-
     if indicateur != "Aucun" : 
-
         fig = px.choropleth(df, geojson=geo_dep, locations='Code_Dep', color=df[indicateur],
                                 hover_name= 'Nom',
                                 title = str(indicateur),
@@ -50,27 +38,23 @@ with viz :
         fig.update_geos(visible=False)
         fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
         st.plotly_chart(fig)
-
-
 with comparatif : 
-
+    #On stocke sous forme de liste autant de départements que l'utilisateur a choisi 
     options = st.multiselect('Choisissez des départements à comparer', df['Nom'])
+    # On met cette condition pour rester dans une logique de comparaison
     if len(options) >= 2 : 
+        # Création d'une table avec nos 6 indicateurs et autant de lignes qu'il y a de departements choisis
         st.table(df.loc[df['Nom'].isin(options)])
-        
-
-
     @st.cache
     def convert_df(df):
-        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        # on rajoute le st.cache pour éviter la computation de la fonction à chaque run de l'app
         return df.to_csv().encode('utf-8')
-
+    # Le principe est simplement d'utiliser la list "options" générée grâce au multiselect 
     csv = convert_df(df.loc[df['Nom'].isin(options)])
-
     st.download_button(
     label="Télecharger le tableau des données comparatives au format CSV",
     data=csv,
-    file_name='large_df.csv',
+    file_name='comparatif_indic_dep.csv',
     mime='text/csv')
 
 
